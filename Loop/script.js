dca827a9a1ba753b174766bf9f2893d056facc6e
Siam Ahmed <rh.siam999@gmail.com>
2025-10-12 00:16:22 +0600

    11-10-25

---
+++ Loop/script.js
@@ -0,0 +1,119 @@
+// const numbers = [12, 98, 45, 63, 21, 72];
+
+// for(const num of numbers) {
+//     console.log(num);
+// }
+
+
+// const fruits = ["orange", "apple", "banana", "jackfruit", "watermelon"];
+
+// for(const item of fruits) {
+//     console.log(item);
+// }
+
+// const subjects = ["math", "english", "bangla", "drawing", "islam"];
+// for(const sub of subjects) {
+//     console.log(sub);
+// }
+
+// const favFoods = ["Burger", "Pizza", "Cold-coffee"];
+// for(const food of favFoods) {
+//     console.log(food);
+// }
+
+// const birthDate = ["1992", "1996", "2004"];
+// for(const date of birthDate) {
+//     console.log(date);
+// }
+
+// const favPlayer = ["Mushi", "Shakib", "Tamim", "Sabbir", "Mustafizur"];
+// for(const player of favPlayer) {
+//     console.log(player);
+// }
+
+
+
+
+// while
+// let num = 0;
+// while(num < 5) {
+//     console.log(num);
+//     // num = num + 1;
+//     num++;
+// }
+
+// let practice = 0;
+// while(practice < 10) {
+//     console.log("Ami potidin 3h programming practice korbo");
+//     practice++;
+// }
+
+
+// let num = 1;
+// while(num <= 20) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 50;
+// while(num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 1;
+// while(num <= 10) {
+//     console.log(num * 3);
+//     num++;
+// }
+
+
+// let num = 111;
+// while(num >= 110) {
+//     console.log(num);
+//     console.log(num / 2);
+//     num--;
+// }
+
+
+
+// let num = 1;
+// let sum = 0;
+// while(num <= 10) {
+//     console.log(num);
+//     sum = sum + num;
+//     num++;
+// }
+// console.log("Sum: ", sum);
+
+
+
+// let num = 50;
+// while(num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num = 5;
+// let sum = 0;
+// while(num <= 15) {
+//     console.log(num);
+//     sum = sum + num;
+//     num++;
+// }
+// console.log("Sum: ", sum);
+
+
+let num = 1;
+let sum = 0;
+
+while(num <= 50) {
+    console.log(num);
+    num++;
+
+    sum = sum + num;
+    console.log("Sum: ", sum);
+}   
