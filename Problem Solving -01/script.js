15a6137e8f5db67350ed724e05f857f3c6c4faa0
Siam Ahmed <rh.siam999@gmail.com>
2025-10-20 01:30:15 +0600

    19-10-25

--- Problem Solving -01/script.js
+++ Problem Solving -01/script.js
@@ -39,9 +39,69 @@ console.log(Height); */
 
 // problem - 04
  
-function mileToGauge(mile) {
-    const gauge = mile * 1760;
-    return gauge;
+// function mileToGauge(mile) {
+//     const gauge = mile * 1760;
+//     return gauge;
+// }
+// const result = mileToGauge(13);
+// console.log(result);
+
+
+
+//problem - 05
+// function convertKwhToCal(kwh) {
+//     const conversionFactor = 860;   // 1kwh = 860 kcal
+//     const kcal = kwh * conversionFactor;  // convert kwh to kcal
+//     return kcal;
+// }
+
+// const kwhOutput = convertKwhToCal(2);
+// console.log(kwhOutput);
+
+
+
+// problem - 06
+/* function hourToSecond(hours) {
+    const seconds = hours * 3600;  // 1hr = 60m, 1min = 60s, 1hr = 60 * 60 = 3600
+    return seconds;
 }
-const result = mileToGauge(13);
-console.log(result);
+
+const result = hourToSecond(5);
+console.log(result); */
+
+
+// problem - 07
+/*function centimeterToMeter(cm) {
+    const m = cm / 100;
+    return m;
+}
+const result = centimeterToMeter(500);
+console.log(result); */
+
+
+// problem - 08
+// function inchToCm(inch) {
+//     const cm = inch * 2.54;
+//     return cm;
+// }
+
+// const result = inchToCm(5);
+// console.log(result);
+
+
+//problem - 09
+// function poundToKilogram(pound) {
+//     const kilogram = pound * 0.453;
+//     return kilogram;
+// }
+// const result = poundToKilogram(5);
+// console.log(result);
+
+
+//problem - 09
+// function gaugeToMeter(yard) {
+//     const meter = yard * 0.91;
+//     return meter;
+// }
+// const result = gaugeToMeter(10);
+// console.log(result);
