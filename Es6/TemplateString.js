0a741a5984fd95ea8236e0a5b6c9ab70c55a48c8
Siam Ahmed <rh.siam999@gmail.com>
2025-10-31 02:35:22 +0600

    10-30-25

---
+++ Es6/TemplateString.js
@@ -0,0 +1,54 @@
+/* const  name = "Siam";
+const age = 30;
+console.log(`Name: ${name}, Age: ${age}`); */
+
+
+/* const a = 10;
+const b = 20;
+
+const math = `The sum of ${a} and ${b} is ${a + b}`;
+console.log(math); */
+
+
+/* const getName = function() {
+    return "Siam";
+}
+const message = `Hello Wahida, This is ${getName()}`;
+console.log(message);
+*/
+
+
+/* const age = 22;
+const status = `You are ${age >= 18 ? "Adult" : "Minor"}`;
+console.log(status); */
+
+
+// const user = { name: "Siam", age: 22};
+// const info = `Name: ${user.name}, Age: ${user.age}`;
+// console.log(info);
+
+
+// const fruits = ["Apple", "Banana", "Mango"];
+// const second = `My second favorite fruit is: ${fruits[1]}`;
+// const list = `My favorite fruits are: ${fruits.join(", ")}`;
+// console.log(list);
+// console.log(second);
+
+
+// html template
+/* const title = "Welcome";
+const body = "This is a dynamic template.";
+const html = `<div>
+    <h1>${title}</h1>
+    <p>${body}</p>
+</div>`;
+
+console.log(html);*/
+
+
+// problem - 01
+function subtract(a, b) {
+    const result = a - b;
+    console.log(`The difference between ${a} and ${b} is ${result} `);
+}
+subtract(100, 20);
---
+++ Es6/script.js
@@ -0,0 +1,38 @@
+// let, const , var difference
+
+// const numbers = [23, 4, 23, 12, 56];
+// numbers[1] = 55;
+// numbers.push(8, 9, 65);
+// console.log(numbers);
+
+
+/* function add(num1, num2 = 0) {
+    const result = num1 + num2;
+    console.log(num1, num2, result);
+    return result;
+}
+
+const sum = add(5);
+console.log(sum);
+const total = add(17, 19);
+console.log(total); */
+
+
+// problem - 01
+/* function sum(a = 0, b = 0, c = 0) {
+    return a + b + c;
+}
+
+console.log(sum(5)); */
+
+
+//problem - 02
+/* function deposit(amount = 50) {
+    console.log(`Deposit ${amount} Tk.`);
+
+}
+deposit();
+deposit(100);
+*/
+
+
